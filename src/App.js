4e2dd4a1057fd1a8ba3bced4e34104012dee49c3
Dawid Holisz <dawid.holisz@gmail.com>
2020-02-20 22:35:57 +0100

    paginacja dla lukasza

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import range from 'lodash/range'
+import classnames from 'classnames'
 
 import './Styles/App.scss'
 
-function App() {
+const pages = Math.round(Math.random() * 69)
+const offset = Math.round(Math.random() * (pages / 3))
+
+const App = () => {
+  const [currentPage, setCurrentPage] = useState(Math.round(Math.random() * pages))
+
+  const nextPage = () => setCurrentPage(currentPage + 1)
+  const prevPage = () => setCurrentPage(currentPage - 1)
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +28,29 @@ function App() {
           Learn React
         </a>
       </header>
+      <div className="pagination">
+        <button className="pagination__nav-button" disabled={currentPage === 1} onClick={prevPage}>
+          prev
+        </button>
+        {range(currentPage - offset, currentPage + offset + 1)
+          .filter(x => x > 0 && x <= pages)
+          .map(x => (
+            <button
+              className={classnames('pagination__page-button', { '-current': x === currentPage })}
+              key={x}
+              onClick={() => setCurrentPage(x)}
+            >
+              {x}
+            </button>
+          ))}
+        <button
+          className="pagination__nav-button"
+          disabled={currentPage === pages}
+          onClick={nextPage}
+        >
+          next
+        </button>
+      </div>
     </div>
   )
 }
